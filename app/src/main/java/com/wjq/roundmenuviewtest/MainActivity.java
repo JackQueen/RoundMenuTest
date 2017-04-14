040a64c6c70811d8c1b70e742b4d16f0c69abe28
JackQueen <457883454@qq.com>
2017-04-14 15:52:03 +0800

    add feature-1

--- app/src/main/java/com/wjq/roundmenuviewtest/MainActivity.java
+++ app/src/main/java/com/wjq/roundmenuviewtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.wjq.roundmenuviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void feature1() {
+        Log.e("feature-1", "add feature-1");
     }
 }
